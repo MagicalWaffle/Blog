c880a7f6eb44b9d06ca0452401ccfb6ee237a0fa
MagicalWaffle <ardit.daku@yahoo.com>
2019-10-23 08:05:08 +0200

    Implemented the comment part of the blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -9,23 +10,23 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post= Post.find(params[:id])
+
   end
 
   def create
-        @post=Post.new(post_params)
+    @post = Post.new(post_params)
 
-        if @post.save
-          #nese posti osh ruajtur bone redirect to the post iteslf
-          redirect_to @post
-        else
-          #nese jo render new apet
-          render 'new'
-        end
+    if @post.save
+      #nese posti osh ruajtur bone redirect to the post iteslf
+      redirect_to @post
+    else
+      #nese jo render new apet
+      render 'new'
+    end
   end
 
   def update
-    @post= Post.find(params[:id])
+
 
     if @post.update(post_params)
       redirect_to @post
@@ -35,21 +36,24 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def destroy
-    @post =Post.find(params[:id])
     @post.destroy
 
     redirect_to posts_path
   end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
   private def post_params
-      #we are requring for our model these specific records or tables rows
-      #it is a security feature of rails tha actually permits these things
-      #from being actually input to the form that we create
-      #if they arent initialized here you can't actually submit data to the server
-    params.require(:post).permit(:title,:content)
+    #we are requring for our model these specific records or tables rows
+    #it is a security feature of rails tha actually permits these things
+    #from being actually input to the form that we create
+    #if they arent initialized here you can't actually submit data to the server
+    params.require(:post).permit(:title, :content)
 
   end
 
